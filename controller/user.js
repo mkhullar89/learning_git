97054815ffb95e91d192d69a9847f5279f3a6088
Manav Khullar <mkhullar89@gmail.com>
2023-03-22 22:42:38 +0530

    Added Some Functions of no use

--- controller/user.js
+++ controller/user.js
@@ -1,6 +1,10 @@
 class UserController{
 
   async createAccountAndWallet(){
-    
+    //new user account with email and password
   }
+  async addwallet(){}
+  async test_wallet(){}
+  async new_wallet()
+  async new_Wallet(){}
 }
