d0b3499d7f07fbf1f7414f448ee1376097b97bba
Manav Khullar <mkhullar89@gmail.com>
2023-03-22 22:59:41 +0530

    Added Data to Dev

--- controller/user.js
+++ controller/user.js
@@ -1,14 +1,11 @@
 class UserController{
 
-<<<<<<< HEAD
   CONST_FOUR
   CONST_FIVE
   CONST_SIX
-=======
   CONST_ONE
   CONST_TWO
   CONST_THREE
->>>>>>> dev
   async createAccountAndWallet(){
     //new user account with email and password
   }
